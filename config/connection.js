317c4009b966e0354610e7ef28c0ed384f434cca
Ethan Willers <willers.enw@gmail.com>
2023-03-13 15:21:08 -0700

    index.js setup

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,10 @@
 // import mongoose
 const { connect, connection } = require('mongoose');
 
-
-// connect method (set useNewParser and useUnifiedTopology to true)
+// connect method (set useUnifiedTopology to true)
 connect('mongodb://localhost/CDN-Api', {
-    useNewParser: true,
     useUnifiedTopology: true,
 });
+
 // export
 module.exports = connection;
