f3188981639841c6a99ed35600429ddb1c9fc632
Ethan Willers <willers.enw@gmail.com>
2023-03-12 21:18:22 -0700

    working on laptop

--- models/Thought.js
+++ models/Thought.js
@@ -1,13 +1,14 @@
 // import mongoose schema and model (also any models needed for population)
 const { Schema, model } = require("mongoose");
-const dayjs = require('dayjs'); 
 // const {  } = require("../models");
 
 // Schema
 const thoughtSchema = new Schema(
   {
-    thoughtText: { type: String, reqired: true, minLength: 1, maxLength: 280 },
+    thoughtText: { type: String, required: true, minLength: 1, maxLength: 280 },
     createdAt: { type: Date, default: Date.now },
+    username: { type: String, required: true },
+    reactions: []
   },
   {
     toJSON: {
@@ -19,7 +20,9 @@ const thoughtSchema = new Schema(
 );
 
 // Virtual properties
-
+thoughtSchema.virtual('reactionCount').get(function () {
+  return
+});
 // Initialize the model
 const Thought = model('thought', thoughtSchema);
 
