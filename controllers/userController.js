baf288b2ad8b86bc47749b3973e318380a03f965
nanea808 <willers.enw@gmail.com>
2023-03-16 03:06:29 -0700

    made user put request

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,18 +23,27 @@ module.exports = {
   // create user using req.param.id
   createUser(req, res) {
     User.create(req.body)
-    .then((user) => res.json(user))
-    .catch((err) => res.status(500).json(err));
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
   },
   // delete user using req.param.id. bonus: delete thoughts related to user
   removeUser(req, res) {
     User.findOneAndDelete({ _id: req.params.id })
-      .then((user) => 
+      .then((user) =>
         !user
-          ? res.status(404).json({ message: 'No user with that ID'})
-          : Thought.deleteMany({ id: { $in: user.thoughts }})
+          ? res.status(404).json({ message: "No user with that ID" })
+          : Thought.deleteMany({ id: { $in: user.thoughts } })
       )
-      .then(() => res.json({ message: 'User and thoughts deleted!'}))
+      .then(() => res.json({ message: "User and thoughts deleted!" }))
       .catch((err) => res.status(500).json(err));
   },
-}
+  updateUser(req, res) {
+    User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 // import methods from controllers
-const { getUsers, getSingleUser, createUser, removeUser } = require('../../controllers/userController');
+const { getUsers, getSingleUser, createUser, removeUser, updateUser } = require('../../controllers/userController');
 
 // set up routes for controller methods ex: router.route('/').get(getMethod).post(createMethod) 
 router.route('/').get(getUsers).post(createUser);
 
-router.route('/:id').get(getSingleUser).delete(removeUser);
+router.route('/:id').get(getSingleUser).delete(removeUser).put(updateUser);
 
 module.exports = router;
