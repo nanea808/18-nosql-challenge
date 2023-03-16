b364e4875901e2dd8830ff1ce5e1e4a036046dae
Ethan Willers <willers.enw@gmail.com>
2023-03-15 18:23:28 -0700

    user routes working

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 // import api routes
 const userRoutes = require('./userRoutes');
 // set up router.use() with imported routes
-router.use('users', userRoutes);
+router.use('/users', userRoutes);
 
 module.exports = router;
